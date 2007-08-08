c80eddfcdb0482504d3d476f1a77d9db17785023
thomas morgan <tm@iprog.com>
2007-08-08 05:22:11 +0000

    fixed typo

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ if %w{test development}.include? ENV['RAILS_ENV']
     end
     alias_method_chain :log_info, :system_call_filtering
   end
-  ActiveRecord::ConnectionAdapters::MySQLAdapter.class_eval do
+  ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
     def log_info_with_system_call_filtering(sql, name, runtime)
       if (sql.nil? || !(sql =~ /SHOW FIELDS/))
         log_info_without_system_call_filtering(sql, name, runtime)
