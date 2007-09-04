1bca14395c0c57dd223e6b88147fb45c28f1e84d
thomas morgan <tm@iprog.com>
2007-09-04 20:47:54 +0000

    changes to support edge

--- init.rb
+++ init.rb
@@ -1,18 +1,22 @@
 if %w{test development}.include? ENV['RAILS_ENV']
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
-    def log_info_with_system_call_filtering(sql, name, runtime)
-      if (sql.nil? || !(sql =~ /pg_(class|index|attribute)/)) && (name != 'PK and serial sequence')
-        log_info_without_system_call_filtering(sql, name, runtime)
+  if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+      def log_info_with_system_call_filtering(sql, name, runtime)
+        if (sql.nil? || !(sql =~ /pg_(class|index|attribute)/)) && (name != 'PK and serial sequence')
+          log_info_without_system_call_filtering(sql, name, runtime)
+        end
       end
+      alias_method_chain :log_info, :system_call_filtering
     end
-    alias_method_chain :log_info, :system_call_filtering
   end
-  ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
-    def log_info_with_system_call_filtering(sql, name, runtime)
-      if (sql.nil? || !(sql =~ /SHOW FIELDS/))
-        log_info_without_system_call_filtering(sql, name, runtime)
+  if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+    ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
+      def log_info_with_system_call_filtering(sql, name, runtime)
+        if (sql.nil? || !(sql =~ /SHOW FIELDS/))
+          log_info_without_system_call_filtering(sql, name, runtime)
+        end
       end
+      alias_method_chain :log_info, :system_call_filtering
     end
-    alias_method_chain :log_info, :system_call_filtering
   end
 end
